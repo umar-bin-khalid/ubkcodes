faf4e7f86ed8f0de54cf30768ae0000cf9b139ac
umar-bin-khalid <umar.ubk@gmail.com>
2026-04-12 20:04:46 +0500

    Add database logging.

--- index-ws.js
+++ index-ws.js
@@ -13,6 +13,15 @@ server.listen(3000, function() {
 	console.log('server started on port 3000');
 });
 
+process.on('SIGINT', () => {
+	wss.clients.forEach(function each(client) {
+		client.close();
+	});
+	server.close(() => {
+		shutdownDB();
+	})
+})
+
 /** Begin Websocket */
 
 const WebSocketServer = require('ws').Server;
@@ -30,6 +39,10 @@ wss.on('connection', function connection(ws) {
 		ws.send('Welcome to my server');
 	}
 
+	db.run(`INSERT INTO visitors (count, time)
+		VALUES (${numClients}, datetime('now'))
+	`)
+
 	ws.on('close', function close() {
 		wss.broadcast(`Current visitors: ${numClients}`);
 		console.log('A client has disconnected');
@@ -42,4 +55,32 @@ wss.broadcast = function broadcast(data) {
 	wss.clients.forEach(function each(client) {
 		client.send(data);
 	})
+}
+
+/** end websockets */
+
+/** begin databases */
+
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+// serializer is just gonna make sure our database is set up before any queries are ready to go.
+db.serialize(() => {
+	db.run(`
+		CREATE TABLE visitors (
+			count INTEGER,
+			time TEXT
+		)
+	`)
+});
+
+function getCount() {
+	db.each("SELECT * FROM visitors", (err, row) => {
+		console.log(row);
+	})
+}
+
+function shutdownDB() {
+	getCount();
+	console.log("Shutting down db");
+	db.close();
 }
